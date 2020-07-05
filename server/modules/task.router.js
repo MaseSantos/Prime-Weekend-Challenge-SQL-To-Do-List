49f423574fa7b6eddfc42bc75c3710ff29c14982
Mase Santos <hola@masesantos.work>
2020-07-05 00:20:28 -0500

    Added Bootstrap and light styling. Created two sections for tasks (incomplete and complete). Added buttons to mark tasks as complete, incomplete, or delete them

--- server/modules/task.router.js
+++ server/modules/task.router.js
@@ -5,7 +5,7 @@ const pool = require('./pool');
 //GET
 router.get('/', (req, res) => {
     console.log('in router /task GET');
-    let queryString = `SELECT * FROM "tasks" ORDER BY "due_date" DESC, "urgency" DESC`;
+    let queryString = `SELECT * FROM "tasks" ORDER BY "done", "due_date" DESC, "urgency" DESC`;
     pool.query(queryString).then((result) => {
         // success
         res.send(result.rows);
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -10,15 +10,38 @@ function getTasks() {
         url: '/task'
     }).then(function (response) {
         console.log('Client GET:', response);
-        let el = $('#viewAllTasks');
+        let el = $('#viewNotDoneTasks');
         el.empty();
+        let el2 = $('#viewDoneTasks');
+        el2.empty();
+        let urgencyLevel;
         for (let i = 0; i < response.length; i++) {
-            el.append(`<tr>
+            if (Number(response[i].urgency) === 4){
+                urgencyLevel = 'ASAP';
+            } else if(Number(response[i].urgency) === 3){
+                urgencyLevel = 'High';
+            } else if(Number(response[i].urgency) === 2){
+                urgencyLevel = 'Medium';
+            } else{
+                urgencyLevel = 'Low';
+            }
+            if (response[i].done === false){
+                el.append(`<tr>
                 <td>${response[i].due_date.split("T")[0]}</td>
                 <td>${response[i].task_name}</td>
                 <td>${response[i].description}</td>
-                <td>${response[i].urgency}</td>
+                <td>${urgencyLevel}</td>
+                <td><button class="btn btn-primary" id="doneButton">Done</button></td>
                 </tr>`)
+            } else{
+                el2.append(`<tr>
+                <td>${response[i].due_date.split("T")[0]}</td>
+                <td>${response[i].task_name}</td>
+                <td>${urgencyLevel}</td>
+                <td><button class="btn btn-primary" id="markAsIncompleteButton">Not Done</button></td>
+                <td><button class="btn btn-primary" id="deleteButton">Delete</button></td>
+                </tr>`)
+            }
         }
     }).catch(function (err) {
         console.log(err);
